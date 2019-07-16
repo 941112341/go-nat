2b31fbbc373387b6f97accc004f8dcc7f65722a9
jiangshihao <jiangshihao@bytedance.com>
2019-07-16 10:43:05 +0800

    反射，不完全
    Change-Id: I94cc01cf0952de687cdfbe5e5161270cadbeaf2d

--- tcp/client_helper.go
+++ tcp/client_helper.go
@@ -1,6 +1,7 @@
 package tcp
 
 import (
+	"encoding/json"
 	"errors"
 	"reflect"
 )
@@ -11,9 +12,23 @@ func Initialize(ip, port, url string, i interface{}) (client *Client, err error)
 	if typ.Kind() != reflect.Func {
 		return nil, errors.New("参数不是函数")
 	}
+	pNumIn := typ.NumIn()
+	iarr := make([]reflect.Value, pNumIn)
 	reflectHandler := func(request *HttpTransferRequest) HttpTransferResponse {
-
-
+		for i := 0; i < pNumIn; i++ {
+			ptyp := typ.In(i)
+			pinst := reflect.New(ptyp).Interface()
+			BinderReq(request, pinst)
+			iarr[i] = reflect.ValueOf(pinst).Elem()
+		}
+		ret := reflect.ValueOf(i).Call(iarr)
+		// 只取第一个参数
+		val := ret[0]
+		h, b := BindResp(val.Interface())
+		body, _ := json.Marshal(b)
+		return HttpTransferResponse{
+			Headers: h, Body: string(body),
+		}
 	}
 	return Connect(ip, port, url, reflectHandler)
 }
